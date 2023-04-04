970ba98630f735bace16a4e71f17f6289cdaf0cd
0-0 <393743721@qq.com>
2023-04-04 16:22:05 +0800

    create project

---
+++ interface/src/main/java/cn/start/ai/App.java
@@ -0,0 +1,20 @@
+package cn.start.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Springboot
+ *
+ */
+
+@SpringBootApplication
+class ApiApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ApiApplication.class,args);
+
+
+    }
+}
